dcb45a7df61cd6d0a0b35dbec0ad7e0ba4356689
gautier-gdx <gautier.gedoux@isep.fr>
2016-04-23 16:58:13 +0200

    assets

--- Example/Pods/Target Support Files/Hexacon/Hexacon-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Hexacon : NSObject
-@end
-@implementation PodsDummy_Hexacon
-@end
--- Example/Pods/Target Support Files/Hexacon/Hexacon-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "Hexacon.h"
-
-FOUNDATION_EXPORT double HexaconVersionNumber;
-FOUNDATION_EXPORT const unsigned char HexaconVersionString[];
-
